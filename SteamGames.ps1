772b55220981ea7056ab2cd675ee76f8d7dfddc2
Ben Neise <ben@neise.co.uk>
2013-05-08 19:45:50 +0100

    Added the existing files

---
+++ SteamGames.ps1
@@ -0,0 +1,4 @@
+﻿# File is from http://steamcommunity.com/id/guruant/games/?xml=1
+#[xml]$BenGames = Get-Content "C:\Users\Ben\Desktop\games.xml"
+[xml]$BenGames = (Invoke-WebRequest "http://steamcommunity.com/id/guruant/games/?xml=1").content
+$bengames.gamesList.games.game | ForEach-Object {$_.Name}
